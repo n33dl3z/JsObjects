694fabad453c19ef3b95428e81e5ed06120749e2
alex <1ziploc2@gmail.com>
2016-09-19 11:48:38 -0600

    added ways to change peramaters

--- js/objects.js
+++ js/objects.js
@@ -18,9 +18,13 @@ var Robot = {
 
     ChangeWeapon: function (equip){
         weapon = equip;
-    }
+    },
 
 }
 
 Robot.speed += parseInt(prompt("Increase Robot speed."));
 alert("Robot now has a speed of " + Robot.speed);
+
+//this next line for the armor is broken. still needs to be fixed.
+Robot.UpgradeArmor(prompt("What should we upgrade the armor to?"));
+alert("Robot armor is now " + Robot.armor);
