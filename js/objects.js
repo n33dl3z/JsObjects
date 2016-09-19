78a2b08cd1e78c488240139100c5399483303169
alex <1ziploc2@gmail.com>
2016-09-19 11:53:18 -0600

    fixed the broken armor prompt

--- js/objects.js
+++ js/objects.js
@@ -13,11 +13,11 @@ var Robot = {
     },
 
     UpgradeArmor: function (don){
-        armor = don;
+        this.armor = don;
     },
 
     ChangeWeapon: function (equip){
-        weapon = equip;
+        this.weapon = equip;
     },
 
 }
@@ -25,6 +25,6 @@ var Robot = {
 Robot.speed += parseInt(prompt("Increase Robot speed."));
 alert("Robot now has a speed of " + Robot.speed);
 
-//this next line for the armor is broken. still needs to be fixed.
+
 Robot.UpgradeArmor(prompt("What should we upgrade the armor to?"));
-alert("Robot armor is now " + Robot.armor);
+alert("Robot armor is now " + Robot.armor + " armor.");
