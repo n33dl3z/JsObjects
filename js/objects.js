858c96ca5b38a5cde47583c712c52eb9522eb9aa
alex <1ziploc2@gmail.com>
2016-09-19 11:36:39 -0600

    made initial robot with attributes

--- js/objects.js
+++ js/objects.js
@@ -1,1 +1,26 @@
 //Objects lesson goes here.
+//commit is a go.
+
+var Robot = {
+    fuel: 100,
+    weapon: "lazer",
+    speed: 10,
+    strength: 5,
+    armor: "light",
+
+    AddFuel: function (tank){
+        fuel += tank;
+    },
+
+    UpgradeArmor: function (don){
+        armor = don;
+    },
+
+    ChangeWeapon: function (equip){
+        weapon = equip;
+    }
+
+}
+
+Robot.speed += parseInt(prompt("Increase Robot speed."));
+alert("Robot now has a speed of " + Robot.speed);
